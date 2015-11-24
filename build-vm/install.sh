5435c9ce6418c293ff23b63f198241257603e8e9
Shabbir Abdul <sabdul@ncsu.edu>
2015-11-24 17:32:54 -0500

    Update install.sh

--- build-vm/install.sh
+++ build-vm/install.sh
@@ -1,11 +1,14 @@
 sudo apt-get update
 sudo wget --no-check-certificate https://github.com/aglover/ubuntu-equip/raw/master/equip_java8.sh && bash equip_java8.sh
 echo "Downloading eclipse, this might take a while ..."
-wget -nv "http://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/mars/1/eclipse-rcp-mars-1-linux-gtk-x86_64.tar.gz&r=1" -O eclipse-rcp-mars-1-linux-gtk-x86_64.tar.gz
+sudo wget -nv "http://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/mars/1/eclipse-rcp-mars-1-linux-gtk-x86_64.tar.gz&r=1" -O eclipse-rcp-mars-1-linux-gtk-x86_64.tar.gz
 echo "Installing eclipse in /opt/eclipse and setting up paths ..."
-(cd /opt && sudo tar xzf /home/vagrant/eclipse-rcp-mars-1-linux-gtk-x86_64.tar.gz)
-sudo ln -s /opt/eclipse/eclipse ~/Desktop/eclipse
+cd /home/vagrant && sudo tar xzf /home/vagrant/eclipse-rcp-mars-1-linux-gtk-x86_64.tar.gz
+sudo ln -s /home/vagrant/eclipse/eclipse /home/vagrant/Desktop/eclipse
+sudo apt-get install -y git
 git clone https://github.com/SoftwareEngineeringToolDemos/FSE-2011-EvoSuite.git /home/vagrant/Desktop/FSE-2011-EvoSuite
-chmod +x /home/vagrant/Desktop/FSE-2011-EvoSuite
-mv /home/vagrant/Desktop/FSE-2011-EvoSuite/build-vm/vm-contents/* /home/vagrant/Desktop/
-rm -rf /home/vagrant/Desktop/FSE-2011-EvoSuite
+sudo chmod a+rwx /home/vagrant/Desktop/FSE-2011-EvoSuite
+sudo mv /home/vagrant/Desktop/FSE-2011-EvoSuite/build-vm/vm-contents/* /home/vagrant/Desktop/
+sudo rm -rf /home/vagrant/Desktop/FSE-2011-EvoSuite
+sudo chown $USER -R /home/vagrant
+sudo chown $USER -R /home/vagrant/Desktop
