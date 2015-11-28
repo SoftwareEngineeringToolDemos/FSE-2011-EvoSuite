4dc693246b4b5126d9137dca39f9dc439f1fd608
Shabbir Abdul <sabdul@ncsu.edu>
2015-11-28 01:43:09 -0500

    Update install.sh

--- build-vm/install.sh
+++ build-vm/install.sh
@@ -1,3 +1,6 @@
+echo "Enabling auto login"
+sudo mkdir /etc/lightdm/lightdm.conf.d
+sudo bash -c 'printf "[SeatDefaults]\nautologin-user=Vagrant" > /etc/lightdm/lightdm.conf.d/50-myconfig.conf'
 sudo apt-get update
 sudo wget --no-check-certificate https://github.com/aglover/ubuntu-equip/raw/master/equip_java8.sh && bash equip_java8.sh
 echo "Downloading eclipse, this might take a while ..."
@@ -9,6 +12,12 @@ sudo apt-get install -y git
 git clone https://github.com/SoftwareEngineeringToolDemos/FSE-2011-EvoSuite.git /home/vagrant/Desktop/FSE-2011-EvoSuite
 sudo chmod a+rwx /home/vagrant/Desktop/FSE-2011-EvoSuite
 sudo mv /home/vagrant/Desktop/FSE-2011-EvoSuite/build-vm/vm-contents/* /home/vagrant/Desktop/
+sudo mv /home/vagrant/Desktop/FSE-2011-EvoSuite/evosuite-1.0.1.jar /home/vagrant/Desktop/EvoSuite
+sudo mkdir /home/vagrant/.config/autostart
+sudo mv /home/vagrant/Desktop/run_script.desktop /home/vagrant/.config/autostart
 sudo rm -rf /home/vagrant/Desktop/FSE-2011-EvoSuite
-sudo chown $USER -R /home/vagrant
-sudo chown $USER -R /home/vagrant/Desktop
+#sudo rm /home/vagrant/Desktop/run_script.desktop
+sudo chown vagrant -R /home/vagrant
+sudo chown vagrant -R /home/vagrant/Desktop
+sudo chmod +x /home/vagrant/Desktop/EvoSuite *
+sudo reboot
